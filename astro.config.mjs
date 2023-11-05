c123b9c49a6b4c5cde37a2bd239274ddf5b1b5d1
Md Tanjil Bhuiyan <tanjilbhuiyan@gmail.com>
2023-11-06 02:06:43 +0600

    Uisng Bun instead Pnpm and using a empty template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,4 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
-
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
-  site: SITE_URL,
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "nord",
-      wrap: false
-    }
-  }
-});
+export default defineConfig({});
--- src/data/config.ts
+++
@@ -1,1 +0,0 @@
-export const SITE_URL = "https://mdtanjilbhuiyan.gitlab.io/";
