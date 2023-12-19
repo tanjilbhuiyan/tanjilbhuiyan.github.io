800c43e3549dd2aa0f5b5542ca08c6284c6565ea
Md Tanjil Bhuiyan <tanjilbhuiyan@gmail.com>
2023-12-20 01:25:07 +0600

    rebase the application

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()]
-});
+export default defineConfig({});
