b4f2f96dddafd95a0045f7d792da7a205d50f653
Md Tanjil Bhuiyan <tanjilbhuiyan@gmail.com>
2024-02-14 05:21:47 +0600

    new file:   tailwind.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import compress from "astro-compress";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), compress()]
+  integrations: [mdx(), sitemap(), tailwind(), compress()]
 });
