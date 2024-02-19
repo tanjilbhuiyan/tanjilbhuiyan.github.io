1d27cee651cc26b5e13ee1e45a057bb9ad95b2c9
Md Tanjil Bhuiyan <tanjilbhuiyan@gmail.com>
2024-02-19 06:59:30 +0600

    Add new files and remove deleted files

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Md. Tanjil Bhuiyan';
-export const SITE_DESCRIPTION = 'Personal Blog & Portfolio Site of Md. Tanjil Bhuiyan';
